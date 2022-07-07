61225fca9ab6647e64d05b234f4fbfb98203711a
whquddn55 <whquddn55@gmail.com>
2022-07-07 09:59:34 +0900

    [Add]: channel join 코드 추가

--- createChannels.sh
+++ createChannels.sh
@@ -19,7 +19,8 @@ function createChannel() {
     PEER_PORT=$4
     CHANNEL_NAME=$5
     ORDERER_PORT=9050 
-    ORDERER_CA=.build/organizations/ordererOrganizations/orderer.aomd.com/orderers/orderer.aomd.com/msp/tlscacerts/tlsca.aomd.com-cert.pem
+    export CORE_PEER_TLS_ENABLED=true
+    export ORDERER_CA=.build/organizations/ordererOrganizations/orderer.aomd.com/orderers/orderer.aomd.com/msp/tlscacerts/tlsca.aomd.com-cert.pem
     export CORE_PEER_LOCALMSPID=$MSP_NAME
     export CORE_PEER_TLS_ROOTCERT_FILE=.build/organizations/$ORG_GROUP_NAME/$ORG_NAME.aomd.com/peers/peer0.$ORG_NAME.aomd.com/tls/ca.crt
     export CORE_PEER_MSPCONFIGPATH=.build/organizations/$ORG_GROUP_NAME/$ORG_NAME.aomd.com/users/Admin@$ORG_NAME.aomd.com/msp
@@ -40,6 +41,34 @@ function createChannel() {
         let rc=$res
 		COUNTER=$(expr $COUNTER + 1)
     done
+    cat log.txt
+
+    joinChannel $CHANNEL_NAME $ORG_NAME $PEER_PORT
+}
+
+function joinChannel() {
+    CHANNEL_NAME=$1
+    ORG_NAME=$2
+    PORT=$3
+
+    FABRIC_CFG_PATH=$PWD
+    BLOCKFILE=$PWD/.build/channel-artifacts/${CHANNEL_NAME}.block
+
+    echo $BLOCKFILE
+
+    local rc=1
+	local COUNTER=1
+	## Sometimes Join takes time, hence retry
+	while [ $rc -ne 0 -a $COUNTER -lt 5 ] ; do
+    sleep 3
+    set -x
+    peer channel join -b $BLOCKFILE >&log.txt
+    res=$?
+    { set +x; } 2>/dev/null
+		let rc=$res
+		COUNTER=$(expr $COUNTER + 1)
+	done
+    cat log.txt
 }
 
 function createChannels() {
@@ -53,6 +82,12 @@ function createChannels() {
     cp config/core.yaml ./core.yaml
 
     createChannel EducationOrg1MSP educationOrganizations educationOrg1 6051 educationchannel
+    # joinChannel educationchannel educationOrg1 6051
+
     createChannel AwardOrg1MSP awardOrganizations awardOrg1 7051 awardchannel
+    # joinChannel licensechannel licenseOrg1 8051
+
     createChannel LicenseOrg1MSP licenseOrganizations licenseOrg1 8051 licensechannel
+    # joinChannel awardchannel awardOrg1 7051
+    
 }
