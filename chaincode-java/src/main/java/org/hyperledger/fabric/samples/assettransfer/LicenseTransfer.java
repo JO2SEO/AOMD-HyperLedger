582a41d2b2bf31ecc37dcd4b28f45c38469c0f24
whquddn55 <whquddn55@gmail.com>
2022-07-08 20:53:59 +0900

    [Add]: LicenseTransfer.java 추가 [Add]: license생성 함수 구현 [Add]: license중복체크 함수 구현

---
+++ chaincode-java/src/main/java/org/hyperledger/fabric/samples/assettransfer/LicenseTransfer.java
@@ -0,0 +1,72 @@
+package org.hyperledger.fabric.samples.assettransfer;
+
+import com.owlike.genson.Genson;
+import org.hyperledger.fabric.contract.Context;
+import org.hyperledger.fabric.contract.ContractInterface;
+import org.hyperledger.fabric.contract.annotation.*;
+import org.hyperledger.fabric.shim.ChaincodeException;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+
+import java.time.LocalDateTime;
+
+@Contract(
+        name = "aomd",
+        info = @Info(
+                title = "Asset Transfer",
+                description = "Aomd asset transfer",
+                version = "1.0.0",
+                license = @org.hyperledger.fabric.contract.annotation.License(
+                        name = "Apache 2.0 License",
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.html"),
+                contact = @Contact(
+                        email = "aomd@gmail.com",
+                        name = "Jo2Seo",
+                        url = "https://aomd.com")))
+public final class LicenseTransfer implements ContractInterface {
+    private final Genson genson = new Genson();
+
+    private enum AssetTransferErrors {
+        ASSET_NOT_FOUND,
+        ASSET_ALREADY_EXISTS
+    }
+
+    @Transaction(intent = Transaction.TYPE.SUBMIT)
+    public void initLedger(final Context context) {
+        ChaincodeStub stub = context.getStub();
+    }
+
+    @Transaction(intent = Transaction.TYPE.SUBMIT)
+    public License createLicense(
+            final Context context,
+            final String id,
+            final String title,
+            final Long ownerId,
+            final String publisher,
+            final LocalDateTime publishedAt,
+            final String description,
+            final LocalDateTime expireDate,
+            final String qualificationNumber
+    ) {
+        ChaincodeStub stub = context.getStub();
+
+        if (exists(context, id)) {
+            String errorMesage = String.format("Asset %s already exists", id);
+            System.out.println(errorMesage);
+            throw new ChaincodeException(errorMesage, AssetTransferErrors.ASSET_ALREADY_EXISTS.toString());
+        }
+
+        License license = new License(id, title, ownerId, publisher, publishedAt, LocalDateTime.now(), description, expireDate, qualificationNumber);
+        String licenseJson = genson.serialize(license);
+        stub.putStringState(id, licenseJson);
+
+        return license;
+    }
+
+    @Transaction(intent = Transaction.TYPE.EVALUATE)
+    public boolean exists(final Context context, final String id) {
+        ChaincodeStub stub = context.getStub();
+        String json = stub.getStringState(id);
+
+        return (json != null && !json.isEmpty());
+    }
+}
