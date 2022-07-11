1a051b99243d4665ca256b62802d2b10bd0022cb
whquddn55 <whquddn55@gmail.com>
2022-07-11 21:11:57 +0900

    [Add]: educationCC 추가

---
+++ chaincode-java/src/main/java/org/hyperledger/fabric/samples/assettransfer/EducationTransfer.java
@@ -0,0 +1,123 @@
+package org.hyperledger.fabric.samples.assettransfer;
+
+import com.owlike.genson.Genson;
+import org.hyperledger.fabric.contract.Context;
+import org.hyperledger.fabric.contract.ContractInterface;
+import org.hyperledger.fabric.contract.annotation.Contact;
+import org.hyperledger.fabric.contract.annotation.Contract;
+import org.hyperledger.fabric.contract.annotation.Info;
+import org.hyperledger.fabric.contract.annotation.Transaction;
+import org.hyperledger.fabric.contract.annotation.Default;
+import org.hyperledger.fabric.shim.ChaincodeException;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+import org.hyperledger.fabric.shim.ledger.KeyValue;
+import org.hyperledger.fabric.shim.ledger.QueryResultsIterator;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Contract(
+        name = "educationCC",
+        info = @Info(
+                title = "Asset Transfer",
+                description = "Aomd asset transfer",
+                version = "1.0.0",
+                license = @org.hyperledger.fabric.contract.annotation.License(
+                        name = "Apache 2.0 License",
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.html"),
+                contact = @Contact(
+                        email = "aomd@gmail.com",
+                        name = "Jo2Seo",
+                        url = "https://aomd.com"
+        )
+    )
+)
+@Default
+public final class EducationTransfer implements ContractInterface {
+    private final Genson genson = new Genson();
+
+    private enum AssetTransferErrors {
+        ASSET_NOT_FOUND,
+        ASSET_ALREADY_EXISTS
+    }
+
+    @Transaction(intent = Transaction.TYPE.SUBMIT)
+    public void init(final Context context) {
+        ChaincodeStub stub = context.getStub();
+
+        create(context, "education1", "title1", 1L, "publisher1", LocalDateTime.now(), "state1", "departmentInfo1");
+        create(context, "education2", "title2", 2L, "publisher2", LocalDateTime.now(), "state2", "departmentInfo2");
+        create(context, "education3", "title3", 3L, "publisher3", LocalDateTime.now(), "state3", "departmentInfo3");
+        create(context, "education4", "title4", 4L, "publisher4", LocalDateTime.now(), "state4", "departmentInfo4");
+        create(context, "education5", "title5", 5L, "publisher5", LocalDateTime.now(), "state5", "departmentInfo5");
+        create(context, "education6", "title6", 6L, "publisher6", LocalDateTime.now(), "state6", "departmentInfo6");
+    }
+
+    @Transaction(intent = Transaction.TYPE.SUBMIT)
+    public Education create(
+            final Context context,
+            final String id,
+            final String title,
+            final Long ownerId,
+            final String publisher,
+            final LocalDateTime publishedAt,
+            final String state,
+            final String departmentInfo
+    ) {
+        ChaincodeStub stub = context.getStub();
+
+        if (exists(context, id)) {
+            String errorMessage = String.format("Asset %s already exists", id);
+            System.out.println(errorMessage);
+            throw new ChaincodeException(errorMessage, AssetTransferErrors.ASSET_ALREADY_EXISTS.toString());
+        }
+
+        Education education = new Education(id, title, ownerId, publisher, publishedAt, LocalDateTime.now(), state, departmentInfo);
+        String educationJson = genson.serialize(education);
+        stub.putStringState(id, educationJson);
+
+        return education;
+    }
+
+    @Transaction(intent = Transaction.TYPE.EVALUATE)
+    public boolean exists(final Context context, final String id) {
+        ChaincodeStub stub = context.getStub();
+        String json = stub.getStringState(id);
+
+        return (json != null && !json.isEmpty());
+    }
+
+    @Transaction(intent = Transaction.TYPE.EVALUATE)
+    public Education read(final Context context, final String id) {
+        ChaincodeStub stub = context.getStub();
+        String json = stub.getStringState(id);
+
+        if (!exists(context, id)) {
+            String errorMessage = String.format("Asset %s does not exists", id);
+            System.out.println(errorMessage);
+            throw new ChaincodeException(errorMessage, AssetTransferErrors.ASSET_ALREADY_EXISTS.toString());
+        }
+        Education education = genson.deserialize(json, Education.class);
+        return education;
+    }
+
+    @Transaction(intent = Transaction.TYPE.EVALUATE)
+    public String getAll(final Context context) {
+        ChaincodeStub stub = context.getStub();
+
+        List<Education> queryResults = new ArrayList<>();
+
+        QueryResultsIterator<KeyValue> results = stub.getStateByRange("", "");
+
+        for (KeyValue result: results) {
+            Education education = genson.deserialize(result.getStringValue(), Education.class);
+            queryResults.add(education);
+            System.out.println(education.toString());
+        }
+
+        final String response = genson.serialize(queryResults);
+
+        return response;
+    }
+}
